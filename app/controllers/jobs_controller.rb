3023a59a59443568f39a7f9da24a685f27853aa2
andy2000xyz <andy2000xyz@hotmail.com>
2017-01-09 13:55:02 +0800

    implement part4

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,7 +5,8 @@ class JobsController < ApplicationController
   end
 
   def index
-    @jobs = Job.all
+    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
+
   end
 
   def new
@@ -27,7 +28,7 @@ class JobsController < ApplicationController
   end
 
   def update
-    @job = find(params[:id])
+    @job = Job.find(params[:id])
     if @job.update(job_params)
       redirect_to jobs_path
     else
@@ -47,6 +48,6 @@ end
 private
 
 def job_params
-  params.require(:job).permit(:title, :description)
+  params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
 
 end
